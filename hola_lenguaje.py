e7818a90d58f1b3cd82dcaae07527060177e258b
Katherine Torres <74274010+Kat-Torres@users.noreply.github.com>
2026-03-18 21:15:44 -0500

    Add language greeting to hola_lenguaje.py

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -3,6 +3,8 @@ import os
 def main():
     nombre = os.getenv("USERNAME")
     print(f"¡Hola, {nombre} desde GITHUB!")
+    lenguaje =  os.getenv("LANGUAGE")
+    print(f"¡lenguaje favorito, {lenguaje}")
 
 if __name__== "__main__":
     main()
